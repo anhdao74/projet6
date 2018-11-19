83bdbb9a9f53e1e0d5aef6a78bbb05d3a85d6bff
anhdao <anhdao.lelievre@gmail.com>
2018-11-19 15:34:57 +0100

    ajout de la table trousse dans la barre de recherche

--- manager/SearchManager.php
+++ manager/SearchManager.php
@@ -3,9 +3,9 @@
 class SearchManager extends Manager {
 
     public function search($q) {
-        $req = $this->pdo->query('SELECT id, title, content, file, file2 FROM article WHERE title LIKE "%'.$q.'%" ORDER BY id
-                UNION 
-                SELECT id_article_trousse, title_article_trousse, content_article_trousse, file_article_trousse , type_article  FROM trousse WHERE title_article_trousse LIKE "%'.$q.'%"');
+        $req = $this->pdo->query('SELECT id, title, content, file, file2 FROM article WHERE title LIKE "%'.$q.'%" ORDER BY id') AND
+        	
+                ('SELECT id_article_trousse, title_article_trousse, content_article_trousse, file_article_trousse , type_article  FROM trousse WHERE title_article_trousse LIKE "%'.$q.'%"');
 
         return $req;
     }
